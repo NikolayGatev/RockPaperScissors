ff832d1604937b1a5b78dd135659142f2426b1fe
NikolayGatev <nikolaygatev@gmail.com>
2023-01-24 14:11:59 +0200

    Add project files.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,32 @@
+﻿Random m = new Random();
+int randomChoose = m.Next(1, 4);
+string computerChose = "";
+switch (randomChoose)
+{
+	case 1:
+		computerChose = "Rock";
+		break;
+		case 2:
+		computerChose = "Paper";
+		break;
+		case 3:
+		computerChose = "Scissors";
+        break;
+}
+Console.Write("Chose [r]ock, [p]aper or [s]cisoors: ");
+string pleyerChoose = Console.ReadLine();
+Console.WriteLine($"The computer chose {computerChose}.");
+if ((pleyerChoose == "r" && computerChose == "Scissors") || (pleyerChoose == "s" && computerChose == "Paper") || (pleyerChoose == "p" && computerChose == "Rock"))
+{
+	Console.WriteLine("You win");
+}
+else if ((pleyerChoose == "r" && computerChose == "Rock") || (pleyerChoose == "s" && computerChose == "Scissors") || (pleyerChoose == "p" && computerChose == "Paper"))
+{
+	Console.WriteLine("This game was a draw");
+}
+else
+{
+	Console.WriteLine("You lost");
+}
+
+
